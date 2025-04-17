61329fffd3e517e3ab1bcd1ed89169787cadcddc
Daca <dacaldes@gmail.com>
2025-04-17 15:04:39 +0200

    Cambios para que funcione el despliegue a github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NOMBRE_DEL_REPO/',
+  base: '/movie-app-spa/',
   plugins: [react()],
 })
